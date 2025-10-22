ae08f4a0bcab4cb23cfd903c64909e1a1c2b424a
rwdled <79921530+rwdled@users.noreply.github.com>
2025-10-22 09:23:22 -0500

    modified:   my-app/package.json 	modified:   my-app/server.js

--- my-app/server.js
+++ my-app/server.js
@@ -171,8 +171,9 @@ app.get('/api/users', async (req, res) => {
   }
 });
 
-// Serve React app for all other routes
-app.get('*', (req, res) => {
+// Serve React app for all other routes (fallback)
+// Use app.use without a path so we don't trigger path-to-regexp parsing issues for '*'
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
